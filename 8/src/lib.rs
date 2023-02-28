cc816a261bd749f5029a60961ad70cd6f3e13521
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2023-02-28 07:45:55 -0800

    Solve day 8 part 1

--- 8/src/lib.rs
+++ 8/src/lib.rs
@@ -7,14 +7,14 @@ const UNSCRAMBLED_SIGNALS_LETTERS: &str = "abcefg cf acdeg acdfg bcdf abdfg abde
 static UNSCRAMBLED_SIGNALS: Lazy<Vec<Signal>> = Lazy::new(|| {
     UNSCRAMBLED_SIGNALS_LETTERS
     .split(" ")
-    .map(|s| Signal::fromString(s))
+    .map(|s| Signal::from_string(s))
     .collect()
 });
 
 struct Signal ([bool; 7]);
 
 impl Signal {
-    fn fromString(s: &str) -> Self {
+    fn from_string(s: &str) -> Self {
         // Treat each letter as a digit. E.g. a -> 0, b -> 1, etc.
         let mut signal = [false; 7];
         for c in s.chars() {
@@ -23,7 +23,7 @@ impl Signal {
         Signal(signal)
     }
 
-    fn toString(&self) -> String {
+    fn to_string(&self) -> String {
         self.0.iter()
             .enumerate()
             .filter(|(_, &b)| b)
@@ -31,12 +31,16 @@ impl Signal {
             .collect()
     }
 
+    fn num_digits(&self) -> i32 {
+        self.0.iter().filter(|&&b| b).count().try_into().unwrap()
+    }
+
     /// Uses the number of on-segments to guess the value of the signal.
     fn possible_value_from_length(&self) -> Vec<i32> {
         let mut possible_values = Vec::new();
 
         for (i, signal) in UNSCRAMBLED_SIGNALS.iter().enumerate() {
-            if signal.0.len() == self.0.len() {
+            if signal.num_digits() == self.num_digits() {
                 possible_values.push(i as i32);
             }
         }
@@ -47,7 +51,7 @@ impl Signal {
 
 impl std::fmt::Debug for Signal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.toString())
+        write!(f, "{}", self.to_string())
     }
 }
 
@@ -58,7 +62,7 @@ struct Scenario {
 }
 
 impl Scenario {
-    fn fromLine(line: &str) -> Option<Self> {
+    fn from_line(line: &str) -> Option<Self> {
         if line.len() == 0 {
             return Option::None;
         }
@@ -70,8 +74,8 @@ impl Scenario {
         }
 
         Option::Some(Self {
-            sample_signals: parts[0].split(" ").map(|s| Signal::fromString(s)).collect(),
-            reading: parts[1].split(" ").map(|s| Signal::fromString(s)).collect(),
+            sample_signals: parts[0].split(" ").map(|s| Signal::from_string(s)).collect(),
+            reading: parts[1].split(" ").map(|s| Signal::from_string(s)).collect(),
         })
     }
 }
@@ -83,7 +87,7 @@ fn parse(filename: &str) -> Vec<Scenario> {
 
     contents
         .split('\n')
-        .map(|line| Scenario::fromLine(line))
+        .map(|line| Scenario::from_line(line))
         .filter_map(|s| s)
         .collect()
 }
@@ -91,9 +95,9 @@ fn parse(filename: &str) -> Vec<Scenario> {
 pub fn solve_pt1(filename: &str) -> i32 {
     let scenarios = parse(filename);
 
-    for scenario in scenarios {
-        println!("Scenario: {:?}", scenario);
-    }
-
-    0
+    scenarios.iter()
+        .flat_map(|s| &s.reading)
+        .map(|r| r.possible_value_from_length().len())
+        .filter(|&x| x == 1)
+        .count().try_into().unwrap()
 }
