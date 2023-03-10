e2aa486e4bcd5210f52d770c9d39dae898a93b40
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2023-03-10 08:23:06 -0800

    10 part 2

--- 10/src/bracket.rs
+++ 10/src/bracket.rs
@@ -47,12 +47,21 @@ impl Bracket {
     }
 
     /// Arbitrary score provided by the problem.
-    pub fn score(&self) -> i32 {
+    pub fn score_if_invalid(&self) -> i32 {
         match self {
             Self::Round => 3,
             Self::Square => 57,
             Self::Curly => 1197,
             Self::Angle => 25137,
+        }
+    }
+
+    pub fn score_if_unmatched(&self) -> i32 {
+        match self {
+            Self::Round => 1,
+            Self::Square => 2,
+            Self::Curly => 3,
+            Self::Angle => 4,
         }
     }
 }
--- 10/src/lib.rs
+++ 10/src/lib.rs
@@ -33,12 +33,47 @@ pub fn solve_pt1(filename: &str) -> i32 {
     contents
         .lines()
         .filter_map(first_invalid_bracket)
-        .map(|b| b.score())
+        .map(|b| b.score_if_invalid())
         .sum()
 }
 
-pub fn solve_pt2(filename: &str) -> i32 {
-    0
+/// Gets the brackets that don't have a closing bracket.
+///
+/// If an invalid closing bracket is found, the line is invalid so we return None.
+fn get_unmatched_brackets(line: &str) -> Option<Vec<Bracket>> {
+    let mut stack = Vec::new();
+
+    for c in line.chars() {
+        if let Some(b) = Bracket::from_opening_char(c) {
+            stack.push(b);
+        }
+        else if let Some(b) = Bracket::from_closing_char(c) {
+            if let Some(top) = stack.pop() {
+                if top != b {
+                    return None;
+                }
+            }
+            else {
+                return None;
+            }
+        }
+    }
+
+    Some(stack)
+}
+
+pub fn solve_pt2(filename: &str) -> u64 {
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    let mut scores: Vec<u64> = contents
+        .lines()
+        .filter_map(get_unmatched_brackets)
+        .map(|brackets| brackets.iter().rev().fold(0, |acc, b| 5 * acc + b.score_if_unmatched() as u64))
+        .collect();
+
+    scores.sort();
+    // Problem guarantees there will be an odd number of scores.
+    scores[scores.len() / 2]
 }
 
 #[cfg(test)]
@@ -52,6 +87,6 @@ mod tests {
 
     #[test]
     fn test_pt2() {
-        assert_eq!(solve_pt2("demo.txt"), 0);
+        assert_eq!(solve_pt2("demo.txt"), 288957);
     }
 }
